809c383e6459ac01b7cc147a0e919c22d769ad21
Arsalan Noghrehkar <arsalan.noghrehkar@gmail.com>
2021-01-04 22:50:30 +0330

    add spring security

--- src/main/java/info/eventium/eventium/EventiumApplication.java
+++ src/main/java/info/eventium/eventium/EventiumApplication.java
@@ -10,20 +10,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
-@EntityScan( basePackages = {"core.entity"})
+@EntityScan(basePackages = {"core.entity"})
 @EnableJpaRepositories(basePackages = "core.repository")
-@ComponentScan(basePackages = {"adapter","core"})
+@ComponentScan(basePackages = {"adapter", "core"})
 @SpringBootApplication
-public class EventiumApplication {
-	@Bean
-	public ModelMapper getObjectMapper(){
-		return new ModelMapper();
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(EventiumApplication.class, args);
-	}
+@EnableWebSecurity
+public class EventiumApplication extends WebSecurityConfigurerAdapter {
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventiumApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/post/**").permitAll()
+                .anyRequest().authenticated();
+
+
+    }
 
 }
